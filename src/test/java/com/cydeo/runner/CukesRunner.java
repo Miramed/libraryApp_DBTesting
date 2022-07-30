d9a20eb69666947c8eb61a7ef8fe71cf74fbf72e
Miramed <mr.tekin2112@gmail.com>
2022-07-30 09:22:19 -0500

    New Project Updated

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -12,6 +12,7 @@ import org.junit.runner.RunWith;
                 "rerun:target/rerun.txt" ,
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
+
         features = "src/test/resources/features" ,
         glue = "com/cydeo/steps",
         dryRun = false,
